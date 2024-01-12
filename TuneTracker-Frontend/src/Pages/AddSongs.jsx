4594ff52a1330177e5af1d70f53b99c7e6def097
Sohana Khatun <83324509+sohanakhatun@users.noreply.github.com>
2024-01-12 21:11:40 +0530

    Update AddSongs.jsx

--- TuneTracker-Frontend/src/Pages/AddSongs.jsx
+++ TuneTracker-Frontend/src/Pages/AddSongs.jsx
@@ -37,7 +37,7 @@ const AddSongs = () => {
       setAlbumart("");
       setArtist("");
       setYear();
-      toast.success("Song added successfully");
+    
       navigate("/");
     } catch (error) {
       toast.error(error);
